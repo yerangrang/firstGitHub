fe7d6d4a53415e633f710142c134215ee89e597a
예린 <eiren_123@naver.com>
2023-11-11 23:23:08 +0900

    this is correct.

--- src/javaProject/FirstGH.java
+++ src/javaProject/FirstGH.java
@@ -5,7 +5,7 @@ public class FirstGH {
 	public static void main(String[] args) {
 
 		System.out.println("this is firstGH");
-//		System.out.println("this is 수정");
+		System.out.println("this is 수정");
 	}
 
 }
