4abf2f7fc5b996761725bf0522a7a4d5fa4c6ee6
예린 <150512735+yerangrang@users.noreply.github.com>
2023-11-11 23:31:24 +0900

    Update FirstGH.java

--- src/javaProject/FirstGH.java
+++ src/javaProject/FirstGH.java
@@ -4,7 +4,7 @@ public class FirstGH {
 
 	public static void main(String[] args) {
 
-		System.out.println("this is firstGH");
+		System.out.println("좋아~!");
 		System.out.println("this is 수정");
 	}
 
