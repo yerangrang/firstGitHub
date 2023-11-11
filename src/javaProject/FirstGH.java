3123717a7b39fd1a7dcf70dd3246841c6ddea074
예린 <eiren_123@naver.com>
2023-11-11 23:03:51 +0900

    this is commit message.

---
+++ src/javaProject/FirstGH.java
@@ -0,0 +1,11 @@
+package javaProject;
+
+public class FirstGH {
+
+	public static void main(String[] args) {
+
+		System.out.println("this is firstGH");
+//		System.out.println("this is 수정");
+	}
+
+}
