d7bac0d09c8adfd79744c6902e866342911ac9e4
예린 <eiren_123@naver.com>
2023-11-11 23:19:00 +0900

    두 번째 커밋!!

---
+++ src/javaProject/FirstGH2.java
@@ -0,0 +1,12 @@
+package javaProject;
+
+public class FirstGH2 {
+
+	public static void main(String[] args) {
+
+		System.out.println("this is secondGH");
+		System.out.println("토큰 개에반데 너무 힘들다");
+//		System.out.println("this is 수정");
+	}
+
+}
